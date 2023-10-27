0c2515aee2babe7719f7d41c9cbc98ad4f9838a1
Jesus Hincapie <jesushincapie76@gmail.com>
2023-10-27 18:41:43 -0500

    Termindo [ sin modal :( ]

--- Examen2/js/mostrar.js
+++ Examen2/js/mostrar.js
@@ -1,12 +1,25 @@
-function ShowHide() {
-    var section2 = document.getElementById("seccion2");
-    var section3 = document.getElementById("section3");
+document.getElementById('verMas').addEventListener('click', function () {
+    if (confirm('¿Deseas continuar?')) {
+        document.getElementById('seccion2').style.display = 'flex';
+        document.getElementById('section3').style.display = 'flex';
+        
+        // Mostrar los enlaces ocultos
+        document.querySelectorAll('.servicios-link, .colaboradores-link').forEach(link => {
+            link.style.display = 'inline'; // o 'block', dependiendo del tipo de display deseado
+        });
+        document.querySelectorAll('.vermas-link').forEach(link => {
+            link.style.display = 'none'; // o 'block', dependiendo del tipo de display deseado
+        });
+    }//si no oprime continuar no hara nada
+});
+const modal=document.querySelector(".modal")
+const botonAbrir=document.getElementById("abrirModal")
+const botonCerrar=document.getElementById("cerrarModal")
+botonAbrir.document.addEventListener("click",abrirModal)
 
-    if (section2.style.display === "none") {
-        section2.style.display = "block";
-        section3.style.display = "block";
-    } else {
-        section2.style.display = "none";
-        section3.style.display = "none";
-    }
+function abrirModal(){
+    modal.classList.add("revelar");
+}
+function cerrarModal(){
+    modal.classList.remove("revelar")
 }
